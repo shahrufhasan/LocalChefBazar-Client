6ef324c52f739b10dafa4f04f4828248ad29b9bb
Shahruf hasan <info.shahrufhasan@gmail.com>
2025-12-09 15:33:35 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello Assignment</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/layout/RootLayout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RootLayout = () => {
+  return <div>Hello From Root</div>;
+};
+
+export default RootLayout;
